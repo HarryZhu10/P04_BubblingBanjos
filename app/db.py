64983411929226544d04815b2a89a46229fd0515
Shinji Kusakabe <skusakabe30@stuy.edu>
2023-05-04 13:38:07 -0400

    Added database python file

---
+++ app/db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+DB_FILE = "data.db"
+
+db = None
+
+def db_connect():
+    global db
+    db = sqlite3.connect(DB_FILE)
+    return db.cursor()
+
+def db_close():
+    db.commit()
+    db.close()
+
+def db_init_table():
+    c = db_connect(DB_FILE)
+    db_close()
