c6ee595eb567a384e2b23fdb13b80aeaf0b005d0
Ivan Yeung <iyeung30@stuy.edu>
2023-05-04 14:02:04 -0400

    Got working flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+from flask import Flask, render_template, request
+import os
+
 app = Flask(__name__)
 
 app.secret_key = os.urandom(12)
@@ -9,4 +12,9 @@ def index():
 @app.route("/map", methods=['GET', 'POST'])
 def map():
     if request.method == 'GET':
-        return render_template('home.html')
+        return render_template('map.html')
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
