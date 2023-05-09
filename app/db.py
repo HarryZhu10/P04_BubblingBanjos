f3453139570e30ec993ad8f50468e44b5d625b3d
Shinji Kusakabe <skusakabe30@stuy.edu>
2023-05-09 14:10:40 -0400

    Worked more on csv stuff

--- app/db.py
+++ app/db.py
@@ -13,11 +13,20 @@ curl = 'https://drive.google.com/uc?id=' + curl.split('/')[-2]
 surl = 'https://drive.google.com/uc?id=' + surl.split('/')[-2]
 aurl = 'https://drive.google.com/uc?id=' + aurl.split('/')[-2]
 durl = 'https://drive.google.com/uc?id=' + durl.split('/')[-2]
+# curl = curl
+# surl = 
 
 # cdf = pd.read_csv(curl, usecols=["CRASH DATE", "CRASH TIME", "ZIP CODE", "LATITUDE", "LONGITUDE", "NUMBER OF PERSONS INJURED", "NUMBER OF PERSONS KILLED"])
-# sdf = pd.read_csv(surl, usecols=[])
-# adf = pd.read_csv(aurl, usecols=[])
-ddf = pd.read_csv(durl, usecols=['Zip Code', 'Female Count', 'Female Percentage', 'Male Count', 'Male Percentage'])
+
+sdf = pd.read_csv(surl, usecols=['OCCUR_DATE', 'Latitude', 'Longitude', 'PERP_AGE_GROUP', 'PERP_SEX', 'PERP_RACE', 'VIC_AGE_GROUP', 'VIC_SEX', 'VIC_RACE'])
+
+adf = pd.read_csv(aurl, usecols=['ARREST_DATE', 'OFNS_DESC', 'LAW_CODE', 'LAW_CAT_CD', 'Longitude', 'Latitude', 'ARREST_PRECINCT', 'AGE_GROUP', 'PERP_SEX', 'PERP_RACE'])
+
+# Missing 'Hispanic or Latinx Count', 'Hispanic or Latinx Percentage', 'Two Spirit (Native American/ First Nations) Count',
+# 'Two Spirit (Native American/ First Nations) Percentage', 'Native Hawaiian or Pacific Islander Count', 'Native Hawaiian or Pacific Islander Percentage'
+ddf = pd.read_csv(durl, usecols=['Zip Code', 'Female Count', 'Female Percentage', 'Male Count', 'Male Percentage', 'Gender Nonconforming Count', 'Gender Nonconforming Percentage'
+                                 , 'American Indian or Alaskan Native Count', 'American Indian or Alaskan Native Percentage', 'Asian Count', 'Asian Percentage',
+                                 'Black or African American Count', 'Black or African American Percentage', 'Multi-race Count', 'Multi-race Percentage', 'White or Caucasian Count', 'White or Caucasian Percentage', 'Middle Eastern and North African Count', 'Middle Eastern and North African Percentage'])
 
 # def db_connect():
 #     global db
