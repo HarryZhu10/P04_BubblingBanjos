c7072d179840e5c5ac7d524ba7643ff244266bb5
Andrew Piatetsky <apiatetsky30@stuy.edu>
2023-05-10 13:59:17 -0400

    makers rkers work now

--- app/static/js/maps.js
+++ app/static/js/maps.js
@@ -1,13 +1,4 @@
-var data = document.getElementById("data");
 
-json_data = JSON.parse(data.innerHTML);
-var statesData = json_data;
-
-console.log(data.innerHTML);
-console.log(statesData);
-console.log('hello!');
-
-// console.log(statesData)
 
 var map = L.map('map').setView([40.70850, -74.00603], 13); //40.70850/-74.00603
 
@@ -17,7 +8,36 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// add the states polygon data to the map
+var data = document.getElementById("data");
+
+json_data = JSON.parse(data.innerHTML);
+var statesData = json_data;
+
+console.log(data.innerHTML);
+console.log(statesData);
+console.log('hello!');
+
 L.geoJson(statesData).addTo(map);
+
+// end of the states polylgon data code
+
+// to add a bunch of markers to the map
+
+var points_data = document.getElementById("points");
+// console.log(points_data);
+json_points = JSON.parse(points_data.innerHTML);
+console.log(json_points[5])
+
+for (let i = 0; i < json_points.length; i++) {
+    // console.log(json_points[i])
+    var mark = L.marker(json_points[i]).addTo(map);
+    // console.log(mark.getLatLng())
+    var popup_string = "<b>Hello world!</b><br>I am a popup.<br>My coordinate is " + mark.getLatLng()["lat"] + ", " + mark.getLatLng()["lng"];
+    mark.bindPopup(popup_string).openPopup();
+  } 
+
+// end of mass markers code
 
 var marker = L.marker([40.70850, -74.00603]).addTo(map);
 
