3b14ad2b95ef588cba40677f1caa5bd20029f4f4
Shinji Kusakabe <skusakabe30@stuy.edu>
2023-05-16 14:06:21 -0400

    more work

--- app/setup.py
+++ app/setup.py
@@ -1,69 +1,74 @@
-import sqlite3, csv, sqlalchemy, gdown, shapefile, fiona #gdown for downloading big google drive files
+import sqlite3, csv, sqlalchemy, gdown, shapefile, fiona, pyproj #gdown for downloading big google drive files
 import pandas as pd
 
 from collections import OrderedDict
 from sqlalchemy import create_engine
-engine = create_engine('sqlite://', echo=False)
-DB_FILE = "data.db"
+# engine = create_engine('sqlite://', echo=False)
+# DB_FILE = "data.db"
+# 
+# db = sqlite3.connect(DB_FILE, check_same_thread = False)
+# c = db.cursor()
+# 
+# c.execute('CREATE TABLE IF NOT EXISTS collision_info (CRASH DATE TEXT, CRASH TIME TEXT, ZIP CODE INTEGER, LATITUDE REAL, LONGITUDE REAL, NUMBER OF PERSONS INJURED INTEGER, NUMBER OF PERSONS KILLED INTEGER)')
+# db.commit()
+# 
+# curl = 'https://drive.google.com/file/d/1DXS_eqGG3AbR1IrdOcWiXc_qPTEbGFHf/view?usp=sharing'
+# aurl = 'https://drive.google.com/file/d/1HtKO8nK2daRjJm1U50pyUZt_SF0cFmQj/view?usp=sharing'
+# curl = 'https://drive.google.com/uc?id=' + curl.split('/')[-2]
+# aurl = 'https://drive.google.com/uc?id=' + aurl.split('/')[-2]
+# 
+# gdown.download(curl)
+# gdown.download(aurl)
+# 
+# 
+# # Collision and arrest data is obtained separately with gdown because they're too big
+# surl = 'https://drive.google.com/file/d/1EJRFulkdL0sKhH2py9YVXH6gfdvNN_6Q/view?usp=sharing'
+# durl = 'https://drive.google.com/file/d/1sGjh289FyxkBNQ-wdruaxxN0k1WTp2xg/view?usp=sharing'
+# surl = 'https://drive.google.com/uc?id=' + surl.split('/')[-2]
+# durl = 'https://drive.google.com/uc?id=' + durl.split('/')[-2]
+# 
+# cdf = pd.read_csv('Motor_Vehicle_Collisions_-_Crashes.csv', usecols=["CRASH DATE", "CRASH TIME", "ZIP CODE", "LATITUDE", "LONGITUDE", "NUMBER OF PERSONS INJURED", "NUMBER OF PERSONS KILLED"], low_memory = False)
+# 
+# sdf = pd.read_csv(surl, usecols=['OCCUR_DATE', 'Latitude', 'Longitude', 'PERP_AGE_GROUP', 'PERP_SEX', 'PERP_RACE', 'VIC_AGE_GROUP', 'VIC_SEX', 'VIC_RACE'])
+# 
+# adf = pd.read_csv('NYPD_ARRESTS_DATA__HISTORIC_.csv', usecols=['ARREST_DATE', 'OFNS_DESC', 'LAW_CODE', 'LAW_CAT_CD', 'Longitude', 'Latitude', 'ARREST_PRECINCT', 'AGE_GROUP', 'PERP_SEX', 'PERP_RACE'])
+# 
+# Missing 'Hispanic or Latinx Count', 'Hispanic or Latinx Percentage', 'Two Spirit (Native American/ First Nations) Count',
+# 'Two Spirit (Native American/ First Nations) Percentage', 'Native Hawaiian or Pacific Islander Count', 'Native Hawaiian or Pacific Islander Percentage'
+# ddf = pd.read_csv(durl, usecols=['Zip Code', 'Female Count', 'Female Percentage', 'Male Count', 'Male Percentage', 'Gender Nonconforming Count', 'Gender Nonconforming Percentage'
+#                                  , 'American Indian or Alaskan Native Count', 'American Indian or Alaskan Native Percentage', 'Asian Count', 'Asian Percentage',
+#                                  'Black or African American Count', 'Black or African American Percentage', 'Multi-race Count', 'Multi-race Percentage', 'White or Caucasian Count', 'White or Caucasian Percentage', 'Middle Eastern and North African Count', 'Middle Eastern and North African Percentage'])
+# 
+# cdf = cdf.dropna()
+# sdf = sdf.dropna()
+# adf = adf.dropna()
+# ddf = ddf.dropna()
+# 
+# cdf['CRASH DATE'] = pd.to_datetime(cdf['CRASH DATE'], format='%m/%d/%Y')
+# sdf['OCCUR_DATE'] = pd.to_datetime(sdf['OCCUR_DATE'], format='%m/%d/%Y')
+# adf['ARREST_DATE'] = pd.to_datetime(adf['ARREST_DATE'], format='%m/%d/%Y')
+# cdf = cdf[(cdf['CRASH DATE'] >= '01/01/2017')]
+# sdf = sdf[(sdf['OCCUR_DATE'] >= '01/01/2017')]
+# adf = adf[(adf['ARREST_DATE'] >= '01/01/2017')]
+# 
+# cdf.to_sql('collision_info', db, if_exists='fail')
+# sdf.to_sql('shooting_info', db, if_exists='fail')
+# adf.to_sql('arrest_info', db, if_exists='fail')
+# ddf.to_sql('demographic_info', db, if_exists='fail')
 
-db = sqlite3.connect(DB_FILE, check_same_thread = False)
-c = db.cursor()
-
-c.execute('CREATE TABLE IF NOT EXISTS collision_info (CRASH DATE TEXT, CRASH TIME TEXT, ZIP CODE INTEGER, LATITUDE REAL, LONGITUDE REAL, NUMBER OF PERSONS INJURED INTEGER, NUMBER OF PERSONS KILLED INTEGER)')
-db.commit()
-
-curl = 'https://drive.google.com/file/d/1DXS_eqGG3AbR1IrdOcWiXc_qPTEbGFHf/view?usp=sharing'
-aurl = 'https://drive.google.com/file/d/1HtKO8nK2daRjJm1U50pyUZt_SF0cFmQj/view?usp=sharing'
-curl = 'https://drive.google.com/uc?id=' + curl.split('/')[-2]
-aurl = 'https://drive.google.com/uc?id=' + aurl.split('/')[-2]
-
-gdown.download(curl)
-gdown.download(aurl)
-
-
-# Collision and arrest data is obtained separately with gdown because they're too big
-surl = 'https://drive.google.com/file/d/1EJRFulkdL0sKhH2py9YVXH6gfdvNN_6Q/view?usp=sharing'
-durl = 'https://drive.google.com/file/d/1sGjh289FyxkBNQ-wdruaxxN0k1WTp2xg/view?usp=sharing'
-surl = 'https://drive.google.com/uc?id=' + surl.split('/')[-2]
-durl = 'https://drive.google.com/uc?id=' + durl.split('/')[-2]
-
-cdf = pd.read_csv('Motor_Vehicle_Collisions_-_Crashes.csv', usecols=["CRASH DATE", "CRASH TIME", "ZIP CODE", "LATITUDE", "LONGITUDE", "NUMBER OF PERSONS INJURED", "NUMBER OF PERSONS KILLED"], low_memory = False)
-
-sdf = pd.read_csv(surl, usecols=['OCCUR_DATE', 'Latitude', 'Longitude', 'PERP_AGE_GROUP', 'PERP_SEX', 'PERP_RACE', 'VIC_AGE_GROUP', 'VIC_SEX', 'VIC_RACE'])
-
-adf = pd.read_csv('NYPD_ARRESTS_DATA__HISTORIC_.csv', usecols=['ARREST_DATE', 'OFNS_DESC', 'LAW_CODE', 'LAW_CAT_CD', 'Longitude', 'Latitude', 'ARREST_PRECINCT', 'AGE_GROUP', 'PERP_SEX', 'PERP_RACE'])
-
-Missing 'Hispanic or Latinx Count', 'Hispanic or Latinx Percentage', 'Two Spirit (Native American/ First Nations) Count',
-'Two Spirit (Native American/ First Nations) Percentage', 'Native Hawaiian or Pacific Islander Count', 'Native Hawaiian or Pacific Islander Percentage'
-ddf = pd.read_csv(durl, usecols=['Zip Code', 'Female Count', 'Female Percentage', 'Male Count', 'Male Percentage', 'Gender Nonconforming Count', 'Gender Nonconforming Percentage'
-                                 , 'American Indian or Alaskan Native Count', 'American Indian or Alaskan Native Percentage', 'Asian Count', 'Asian Percentage',
-                                 'Black or African American Count', 'Black or African American Percentage', 'Multi-race Count', 'Multi-race Percentage', 'White or Caucasian Count', 'White or Caucasian Percentage', 'Middle Eastern and North African Count', 'Middle Eastern and North African Percentage'])
-
-cdf = cdf.dropna()
-sdf = sdf.dropna()
-adf = adf.dropna()
-ddf = ddf.dropna()
-
-cdf['CRASH DATE'] = pd.to_datetime(cdf['CRASH DATE'], format='%m/%d/%Y')
-sdf['OCCUR_DATE'] = pd.to_datetime(sdf['OCCUR_DATE'], format='%m/%d/%Y')
-adf['ARREST_DATE'] = pd.to_datetime(adf['ARREST_DATE'], format='%m/%d/%Y')
-cdf = cdf[(cdf['CRASH DATE'] >= '01/01/2017')]
-sdf = sdf[(sdf['OCCUR_DATE'] >= '01/01/2017')]
-adf = adf[(adf['ARREST_DATE'] >= '01/01/2017')]
-
-cdf.to_sql('collision_info', db, if_exists='fail')
-sdf.to_sql('shooting_info', db, if_exists='fail')
-adf.to_sql('arrest_info', db, if_exists='fail')
-ddf.to_sql('demographic_info', db, if_exists='fail')
-
-Do a lot of shapefile stuff in order to convert the zipcodes into geographic coordinate data that can be made into polygons.
-Host Zip_Code.shp file on google drive and download it here
-sf = shapefile.Reader('ZIP_CODE_040114.shp')
+# Do a lot of shapefile stuff in order to convert the zipcodes into geographic coordinate data that can be made into polygons.
+# sf = shapefile.Reader('ZIP_CODE_040114.shp')
 
 zipcodeurl = 'https://drive.google.com/file/d/17d8We1qNCsRIm7eH6P9qcBUsQ9iGNhmZ/view?usp=sharing'
 zipcodeurl = 'https://drive.google.com/uc?id=' + zipcodeurl.split('/')[-2]
 gdown.download(zipcodeurl)
 sf = shapefile.Reader('ZIP_CODE_040114.zip')
+
+# source_crs = pyproj.CRS.from_string('PROJCS["NAD_1983_StatePlane_New_York_Long_Island_FIPS_3104_Feet",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Lambert_Conformal_Conic"],PARAMETER["False_Easting",984250.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-74.0],PARAMETER["Standard_Parallel_1",40.66666666666666],PARAMETER["Standard_Parallel_2",41.03333333333333],PARAMETER["Latitude_Of_Origin",40.16666666666666],UNIT["Foot_US",0.3048006096012192]]')
+# target_crs = pyproj.CRS.from_string('EPSG:4326')
+# 
+# transformer = pyproj.Transformer.from_crs(source_crs, target_crs)
+
 with fiona.open('zip://ZIP_CODE_040114.zip') as src:
     asdict = OrderedDict()
     asdict['type'] = 'FeatureCollection'
@@ -72,9 +77,16 @@ with fiona.open('zip://ZIP_CODE_040114.zip') as src:
         feature_dict = OrderedDict()
         feature_dict['type'] = 'Feature'
         feature_dict['geometry'] = feature['geometry']
+        testgeo = feature['geometry']
+        print(testgeo['type'])
+        print(testgeo['coordinates'])
+        testcord = testgeo['coordinates'][0]
+        new_cords = [transformer.transform(x, y) for x, y in testcord]
+        lon, lat = new_cords
+        print(lon + " " + lat)
         feature_dict['properties'] = feature['properties']
         asdict['features'].append(feature_dict)
         
 print(asdict)
 
-db.commit()
+# db.commit()
