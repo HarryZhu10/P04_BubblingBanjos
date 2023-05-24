2b5d5b5e9c5951cc11bed1ff9fc73d1a58e33e7d
Ivan Yeung <iyeung30@stuy.edu>
2023-05-23 22:11:26 -0400

    have basic marker showing on the map

--- app/static/js/maps.js
+++ app/static/js/maps.js
@@ -2,27 +2,6 @@
 
 var map = L.map('map').setView([40.70850, -74.00603], 13); //40.70850/-74.00603
 
-function getColor(d) {
-    return d > 1000 ? '#800026' :
-    d > 500  ? '#BD0026' :
-    d > 200  ? '#E31A1C' :
-    d > 100  ? '#FC4E2A' :
-    d > 50   ? '#FD8D3C' :
-    d > 20   ? '#FEB24C' :
-    d > 10   ? '#FED976' :
-               '#FFEDA0';
-}
-
-function style(feature) {
-    return {
-        fillcolor: getColor(feature.properties.density), //<-- We replace .properties.density with what ever data we will be looking at such as 
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-} //this function will set the color basedn
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -39,9 +18,9 @@ console.log(data.innerHTML);
 console.log(statesData);
 console.log('hello!');
 
-// L.geoJson(statesData).addTo(map);
+L.geoJson(statesData).addTo(map);
 
-L.geoJson(statesData, {style: style}).addTo(map);
+// L.geoJson(statesData, {style: style}).addTo(map);
 
 // end of the states polylgon data code
 
@@ -66,28 +45,33 @@ var yellow = "invert(12%) sepia(83%) saturate(6686%) hue-rotate(1deg) brightness
 var purple = "invert(7%) sepia(100%) saturate(7307%) hue-rotate(247deg) brightness(113%) contrast(145%)";
 var alien = "invert(32%) sepia(35%) saturate(504%) hue-rotate(78deg) brightness(91%) contrast(93%)"
 
+//color values retrieved with the help of https://codepen.io/sosuke/pen/Pjoqqp (CSS filter generator to convert from black to target hex color)
+
 const colorList = [normal, orange, red, yellow, purple, alien];
 
 
-var testGroup =  L.layerGroup().addTo(map); //create a layer allows us to put markers into different groups for easier customization
-
-for (let i = 0; i < json_points.length; i++) {
-    console.log(json_points[i])
-    var mark = L.marker(json_points[i]).addTo(testGroup);
-    mark.getElement().style.filter = normal;
-    // console.log(mark.getLatLng())
-    var popup_string = "<b>Hello world!</b><br>I am a popup.<br>My coordinate is " + mark.getLatLng()["lat"] + ", " + mark.getLatLng()["lng"];
-    mark.bindPopup(popup_string).openPopup();
-  } 
-
 
 /* ================================================================================ PLOTTING POINTS ON MAP ================================================================================ */
-// var collisionGroup = L.layerGroup().addTo(map);
-// for (let i = 0; i < 10; i++) {
-//     var location = {json_col_points[i]., json_col_point[i][2]};
-//     var mark = L.marker(location).addTo(collisionGroup);
-//     mark.getElement().style.filter = normal;
-// }
+// console.log("COLLISION: " + json_col_points[1]);
+var collisionGroup = L.layerGroup().addTo(map);
+for (let i = 0; i < json_col_points.length; i++) {
+    if (json_col_points[i][3].substring(0,9) == "2022-05-1" ) {
+        var mark = L.marker([json_col_points[i][1], json_col_points[i][2]]).addTo(collisionGroup);
+        mark.getElement().style.filter = normal;
+        var popup_string = "<b>Additional Info</b><br> <b>Coordinates:</b> [" + json_col_points[i][1] + ", " + json_col_points[i][2] + "]<br> <b>Crash Time:</b> " + json_col_points[i][4] + "<br><b>Number of Persons Injured:</b> " + json_col_points[i][6] + "<br><b>Number of Persons Killed:</b> " + json_col_points[i][7] + "<br> <b>Vehicle Type:</b> " + json_col_points[i][8];
+        mark.bindPopup(popup_string);
+    }
+}
+
+var shootingsGroup = L.layerGroup().addTo(map);
+for (let i = 0; i < json_sho_points.length; i++) {
+    if (json_sho_points[i][3].substring(0,9) == "2022-05-1") {
+        var mark = L.marker([json_sho_points[i][1], json_sho_points[i][2]]).addTo(shootingsGroup);
+        mark.getElement().style.filter = normal;
+        var popup_string = "<b>Additional Info</b><br> <b>Coordinates:</b> [" + json_sho_points[i][1] + ", " + json_sho_points[i][2] + "]<br> <b>Perpetrator's Age Group:</b> " + json_sho_points[i][4] + "<br><b>Perpetrator's Sex:</b> " + json_sho_points[i][5] + "<br><b>Perpetrator's Race:</b>" + json_sho_points[i][6]+ "<br><b>Victim's Age Group:</b> " + json_sho_points[i][7] + "<br><b>Victim's Sex:</b> " + json_sho_points[i][8] + "<br><b>Victim's Race:</b> " + json_sho_points[i][9];
+        mark.bindPopup(popup_string);
+    }
+}
 
 /* ================================================================================ NAV BAR SCRIPT ================================================================================ */
 
@@ -95,27 +79,25 @@ for (let i = 0; i < json_points.length; i++) {
 //script for changing the color of markers
 
 var selectDropdownCollision = document.getElementById('colorSelector1');
-selectDropdown.addEventListener('input', function() {
-    testGroup.eachLayer(function(marker) {
-        marker.getElement().style.filter = colorList[selectDropdown.value];
+selectDropdownCollision.addEventListener('input', function() {
+    collisionGroup.eachLayer(function(marker) {
+        marker.getElement().style.filter = colorList[selectDropdownCollision.value];
     });
 });
 
-var selectDropdownShootings = document.getElementById('colorselector2');
-selectDropdown.addEventListener('input', function() {
-    //content
+var selectDropdownShootings = document.getElementById('colorSelector2');
+selectDropdownShootings.addEventListener('input', function() {
+    shootingsGroup.eachLayer(function(marker) {
+        marker.getElement().style.filter = colorList[selectDropdownShootings.value];
+    });
 });
 
-var selectDropdownArrests = document.getElementById('colorSelector3');
-selectDropdown.addEventListener('input', function() {
-    //content
-});
 
 //script for displaying/hiding markers
 
 var displayBtnCollision = document.getElementById('collisionDisplay1');
 displayBtnCollision.addEventListener('input', () => {
-    testGroup.eachLayer(function(marker) {
+    collisionGroup.eachLayer(function(marker) {
         marker.getElement().style.visibility = 'visible';
         marker._shadow.style.visibility = 'visible';
     });
@@ -123,7 +105,7 @@ displayBtnCollision.addEventListener('input', () => {
 
 var hideBtnCollison = document.getElementById('collisionDisplay2');
 hideBtnCollison.addEventListener('input', () => {
-    testGroup.eachLayer(function(marker) {
+    collisionGroup.eachLayer(function(marker) {
         marker.getElement().style.visibility = 'hidden';
         marker._shadow.style.visibility = 'hidden';
     });
@@ -131,52 +113,22 @@ hideBtnCollison.addEventListener('input', () => {
 
 var displayBtnShootings = document.getElementById('shootingsDisplay1');
 displayBtnShootings.addEventListener('input', () => {
-    //content
+    shootingsGroup.eachLayer(function(marker) {
+        marker.getElement().style.visibility = 'visible';
+        marker._shadow.style.visibility = 'visible';
+    });
 });
 
 var hideBtnShootings = document.getElementById('shootingsDisplay2');
 hideBtnShootings.addEventListener('input', () => {
-    //content
-});
-
-var displayBtnArrests = document.getElementById('arestsDisplay1');
-displayBtnArrests.addEventListener('input', () => {
-    //content
-});
-
-var hideBtnArrests = document.getElementById('arrestDisplay2');
-hideBtnArrests.addEventListener('input', () => {
-    //content
+    shootingsGroup.eachLayer(function(marker) {
+        marker.getElement().style.visibility = 'hidden';
+        marker._shadow.style.visibility = 'hidden';
+    });
 });
 
 
-// end of mass markers code
 
-var marker = L.marker([40.70850, -74.00603]).addTo(map);
-marker.getElement().style.filter =  "invert(62%) sepia(22%) saturate(2096%) hue-rotate(1deg) brightness(104%) contrast(104%)";
-//<- can change the color of the marker by altering attribute of style.filter
-// examples of style.filter attributes: none | blur() | brightness() | contrast() | drop-shadow() | grayscale() | hue-rotate() | invert() | opacity() | saturate() | sepia()
-
-var circle = L.circle([40.72850, -74.00603], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
-
-var polygon = L.polygon([
-    [40.7819063, -73.9829206],
-    [40.7704018,-73.9915037],
-    [40.7639338,-73.9716339]
-], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-}).addTo(map);
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
 
 function onMapClick(e) {
     console.log("You clicked the map at " + e.latlng);
